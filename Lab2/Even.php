2ebcab681a69cfba35445266e4cd12e047ffb2a5
Eng/Mohammed <mohammed739219037@gmail.com>
2025-12-14 22:45:44 +0300

    Add files via upload

---
+++ Lab2/Even.php
@@ -0,0 +1,9 @@
+<?php
+for ($i = 1; $i <= 10; $i++) {
+    if ($i % 2 == 0) {
+        echo "<span style='color: blue;'>$i</span><br>";
+    } else {
+        echo "$i<br>";
+    }
+}
+?>
---
+++ Lab2/loop_practice.php
@@ -0,0 +1,7 @@
+<?php
+
+$students = ["Mohammed", "Ali", "Abduallh", "Ahmed", "Anis"];
+foreach ($students as $student) {
+    echo "نتمنى لك حظاً سعيداً في الامتحان$student. مرحبا<br>";
+}
+?>
